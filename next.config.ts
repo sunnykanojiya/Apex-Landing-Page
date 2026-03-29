f6099ef4c790e0c248d84d4c486b299cbe3e0d32
utkarshkumarsingh120903@gmail.com <utkarshkumarsingh120903@gmail.com>
2026-03-29 23:09:57 +0530

    updated config file for deployement

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: { unoptimized: true }
+}
 export default nextConfig;
